d4a9fdb5502fa30265c3bdbcd2e0ed2cd3a1035c
Kent Roth <kentencroth@yahoo.com>
2020-10-25 15:23:50 -0500

    Displaying photos to page

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
+const imageContainer = document.getElementById('image-container');
+const loading = document.getElementById('loading-spinner');
+
+let photosArray = [];
+
 const count = 10;
 const apiKey = config.unsplash_key;
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function setAttributes(element, attributes) {
+	for (const key in attributes) {
+		element.setAttribute(key, attributes[key]);
+	}
+}
+
+function displayPhotos() {
+	photosArray.forEach((photo) => {
+		// Creating anchor tag
+		const item = document.createElement('a');
+		setAttributes(item, {
+			href: photo.links.html,
+			target: '_blank',
+		});
+
+		// Creating Image tag
+		const image = document.createElement('img');
+		setAttributes(image, {
+			src: photo.urls.regular,
+			alt: photo.alt_description,
+			title: photo.alt_description,
+		});
+
+		item.appendChild(image);
+		imageContainer.appendChild(item);
+	});
+}
+
 async function getPhotos() {
 	try {
 		const response = await fetch(apiURL);
-		const data = await response.json();
-		console.log(data);
+		photosArray = await response.json();
+		displayPhotos();
 	} catch (error) {}
 }
 
